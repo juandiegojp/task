1382e82838a95b4ffb058f1948713f7575bfbb35
Juan Diego <juandiego.jurado@iesdonana.com>
2023-03-22 20:00:08 +0100

    urls modificadas y eventos

--- task/models.py
+++ task/models.py
@@ -15,4 +15,15 @@ class Task(models.Model):
     
     def __str__(self):
         return f'{self.title} - {self.user.username}'
+
+class Events(models.Model):
+    title = models.CharField(max_length=100)
+    endtime = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE
+    )
+    
+    def __str__(self):
+        return f'{self.title}'
     
--- task/urls.py
+++ task/urls.py
@@ -13,12 +13,19 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
+
 from django.urls import path, include
 from task import views
 
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('task.urls')),
+urlpatterns = [    
+    path('', views.home, name="home"),
+    path('tasks/', views.tasks, name="tasks"),
+    path('tasks/completed/', views.tasks_completed, name="task_completed"),
+    path('task/create', views.create_task, name="create_task"),
+    path('task/<int:task_id>/', views.task_detail, name="task_detail"),
+    path('task/<int:task_id>/complete', views.task_complete, name="task_complete"),
+    path('task/<int:task_id>/delete', views.task_delete, name="task_delete"),
+    path('signup/', views.signup, name="signup"),
+    path('signin/', views.signin, name="signin"),
+    path('logout/', views.signout, name="logout"),
 ]
-
